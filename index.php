390220d0d7c83b74d84aca94c2ad39c762e5547f
Tehnikumi Opilane <opilane@tptlive.ee>
2018-01-22 14:12:21 +0200

    Massiivi töötlemine

--- index.php
+++ index.php
@@ -14,13 +14,55 @@ Massiiv (php ja 35 - array, phyton - list)
 */
 $massiiv = array('Laura',1974,4.11,'Koiduste' );
 
-echo "$tervitus maailm!<br>";
-echo "$tervitus maailm!<br>";
-echo $massiiv[0];
+$esta = 'Eesti keel';
+$mat = 'Matemaatikaa';
+$fyss = 'Füüsika';
 
-if ($bool == true){
-  echo $massiiv;
-} else { echo "Tõeäärtus ei vasta nõutule"; }
+$hinded = array('Peeter'=>array($esta=>4, $mat=>4, $fyss=>3),
+          'Mari'=>array($esta=>5, $mat=>4, $fyss=>3));
+
+ echo "$tervitus maailm!<br>";
+echo "$tervitus maailm!<br>";
+// echo $massiiv [0]. '<br>>'
+
+// Tingimuslause
+if ($bool == False){
+  // massiivi xisu näitamine
+  //print_r($massiiv)
+  for ($i=0; $i < count ($massiiv); $i++) {
+    echo $massiiv [$i]. '<br>';
+  }
+  // echo $massiiv;
+} else { echo "Tõeäärtus ei vasta nõutule";}
+$hinded_id = array_keys($hinded);
+
+for ($i = 0; $i < count ($hinded); $i++){
+  echo $hinded_id[$i]. '<br>';
+foreach ($hinded[$hinded_id[$i]] as $aine => $hinne){
+  echo $aine." : ".$hinne. '<br>';
+  #code
+  }
+}
+
+
+
+$file = fopen ('counter.txt', "a") or die ("Ei saanud faili avada");
+if (!$file){
+$txtx =1;
+fwrite ($file,$txt);
+fclose($file);}
+else {
+  $txt =fread (file, filesize ('countertxt'));
+  $new_txt = $txt + 1;
+  fwrite ($file,$fnew_txt);
+  fclose($file);
+}
+
+$file = fopen('counter.txt', "r") or die("Ei saanuds faili avada");
+echo fread ($file, filsize ('counter.txt'))
+fclose($file);
+
+
 
 
 
